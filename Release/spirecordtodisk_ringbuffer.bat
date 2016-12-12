80b5ca1563abad44768a8610d01d853b3d65f80c
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-12 11:31:31 -0500

    first commit

---
+++ Release/spirecordtodisk_ringbuffer.bat
@@ -0,0 +1,8 @@
+spirecordtodisk_ringbuffer.exe "recording.wav" 3600 "E-MU ASIO" 2 3
+rem spirecordtodisk_ringbuffer.exe "recording.wav" 120 "Wave (2- E-MU E-DSP Audio Proce"
+rem spirecordtodisk_ringbuffer.exe "recording.wav" 7200 "Primary Sound Capture Driver"
+rem spirecordtodisk_ringbuffer.exe "recording.wav" 120 "Wave (2- E-MU E-DSP Audio Processor (WDM))"
+rem spirecordtodisk_ringbuffer.exe "recording.wav" 120 "Speakers (2- E-MU E-DSP Audio Processor (WDM))"
+rem spirecordtodisk_ringbuffer.exe "recording.wav" 7200 "Wave (2- E-MU E-DSP Audio Processor (WDM))"
+rem spirecordtodisk_ringbuffer.exe "recording.wav" 120 "E-DSP Wave [FFC0]"
+rem spirecordtodisk_ringbuffer.exe "recordingtranceriffer.wav" 3600 "Microphone (Fast Track)"
---
+++ Release/spirecordtodisk_ringbuffer1.bat
@@ -0,0 +1,2 @@
+spirecordtodisk_ringbuffer.exe "recording three.wav" 7200 "E-MU ASIO" 2 3
+rem spirecordtodisk_ringbuffer.exe recording.wav 7200 "E-MU ASIO" 2 3
